1e75898b20ec46521d62603a1714f50558df1194
Shao-Chenlei <shao.chenlei@microfocus.com>
2020-05-16 21:05:11 +0800

    update MongoDB connection password

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const{ApolloServer} = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
@@ -6,27 +6,41 @@ const Post = require('./models/Post');
 const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-      getPosts
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
-    Query: {
-        sayHi: () => 'Hello World!'
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
     }
+  }
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers
 });
 
-mongoose.connect(MONGODB,{ useNewUrlParser:true})
-.then(() => {
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({port:5000});
-})
-.then((res) =>{
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
     console.log(`Server running at ${res.url}`);
-});
+  });
