fb49fc314cb8fa7cb6586a3fa209231abbd57344
shaochenlei <946531424@qq.com>
2020-05-25 00:18:11 +0800

    finish lesson 1

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://chenlei:chenlei@cluster0-ge3rz.mongodb.net/test?retryWrites=true&w=majority'
+    MONGODB: 'mongodb+srv://chenlei:chenlei@cluster0-ge3rz.mongodb.net/merng?retryWrites=true&w=majority'
 }
