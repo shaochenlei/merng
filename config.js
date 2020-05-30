a871dc9cbfc9c082fd8bd87a33802dc3c72f5ed0
shaochenlei <946531424@qq.com>
2020-05-30 22:16:03 +0800

    #2:Register & Login Users

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://chenlei:chenlei@cluster0-ge3rz.mongodb.net/merng?retryWrites=true&w=majority'
-}
+    MONGODB: 'mongodb+srv://chenlei:chenlei@cluster0-ge3rz.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY:'some very secret key'
+};
