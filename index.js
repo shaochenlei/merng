963803d78cd384126a074069a73597b88a6df84f
shaochenlei <946531424@qq.com>
2020-05-31 00:07:32 +0800

    #3 Authentication Middleware & create/delete Post

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers')
+const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({ req }) => ({ req })
 });
 
 mongoose
