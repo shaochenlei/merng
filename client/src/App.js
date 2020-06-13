a53dc863d9f6c9f0b02863f233babb4d6a06e5bc
shaochenlei <946531424@qq.com>
2020-06-13 17:38:34 +0800

    #4 Login & Register Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
@@ -10,16 +9,17 @@ import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+
 function App() {
   return (
-    <Container>
-      <Router>
+    <Router>
+      <Container>
         <MenuBar />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-      </Router>
-    </Container>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
